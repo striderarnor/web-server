79eb011f7673fccf7ab58e8554defbd481fbb548
Strider Arnor <=>
2017-03-05 12:55:57 -0500

    refactor middleware

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var middleware = require('./middleware.js');
 
-var middleware = {
-	requireAuthentication: function(req,res,next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req,res,next){
-		console.log('Request - '+ new Date().toString()+req.method+'  '+req.originalUrl);
-		next();
-	}
-};
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
@@ -23,6 +14,3 @@ app.use(express.static(__dirname+'\\public'));
 app.listen(port, function(){
 	console.log('express server started on port: '+port);
 });
-
-
-
